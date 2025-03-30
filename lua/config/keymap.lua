2b62febbf754e1c0cbdebdbacb2a621c49a2ad3d
Tharun K <tharun9832@gmail.com>
2025-03-30 17:59:56 +0530

    Add more keymaps

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,20 +1,24 @@
-vim.g.mapleader = " "
-
 -- Open netrw
 vim.keymap.set("n", "<leader>ft", vim.cmd.Ex)
+
+-- Minimal number of screen lines to keep above and below the cursor
+vim.opt.scrolloff = 10
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Move selection up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Tab management
-vim.keymap.set("n", "te", ":tabedit<Return>")
-vim.keymap.set("n", "<tab>", ":tabnext<Return>")
-vim.keymap.set("n", "<s-tab>", ":tabprev<Return>")
+vim.keymap.set("n", "tn", ":tabnew<CR>")
+vim.keymap.set("n", "<tab>", ":tabnext<CR>")
+vim.keymap.set("n", "<s-tab>", ":tabprev<CR>")
 
 -- Split windows
-vim.keymap.set("n", "<leader>ws", ":split<Return>")
-vim.keymap.set("n", "<leader>wv", ":vsplit<Return>")
+vim.keymap.set("n", "<leader>ws", ":split<CR>")
+vim.keymap.set("n", "<leader>wv", ":vsplit<CR>")
 
 -- Move between windows
 vim.keymap.set("n", "<leader>wh", "<C-w>h")
